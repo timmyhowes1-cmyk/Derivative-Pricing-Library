e3bcd19a9b1f0a4ede6343b8f14f0163338d8c57
timhowes <timmyhowes1@gmail.com>
2026-03-27 13:49:57 +0000

    Adding BS tests

--- tests/test_black_scholes_model.py
+++ tests/test_black_scholes_model.py
@@ -14,7 +14,7 @@ from engines.analytical import BSMAnalyticalEngine
 )
 
 def test_call_prices(vanilla_call, bs_analytical_engine, spot, expected):
-    model = BlackScholes(x0=spot, vol=0.2, r=0.01, q=0.01)
+    model = BSM(x0=spot, vol=0.2, r=0.01, q=0.01)
     price = bs_analytical_engine.get_price(vanilla_call, model)["value"]
     assert price == pytest.approx(expected, abs=1e-10)
 
@@ -22,13 +22,13 @@ def test_call_prices(vanilla_call, bs_analytical_engine, spot, expected):
 @pytest.mark.parametrize(
     "spot, expected",
     [
-        (7.0, 0.0245641162),
+        (7.0, 2.9947136175),
         (10.0, 0.7886308735),
-        (13.0, 3.0700328188),
+        (13.0, 0.0998833176),
     ],
 )
 
-def test_call_prices(vanilla_call, bs_analytical_engine, spot, expected):
-    model = BlackScholes(x0=spot, vol=0.2, r=0.01, q=0.01)
-    price = bs_analytical_engine.get_price(vanilla_call, model)["value"]
+def test_put_prices(vanilla_put, bs_analytical_engine, spot, expected):
+    model = BSM(x0=spot, vol=0.2, r=0.01, q=0.01)
+    price = bs_analytical_engine.get_price(vanilla_put, model)["value"]
     assert price == pytest.approx(expected, abs=1e-10)
